3b705f007e44907d0da83c8829bd4c3024342ee3
Akhila Joseph <43929349+AkhilaJoseph@users.noreply.github.com>
2024-09-26 12:11:19 +1000

    Update build-image.sh

--- 9.2D-resources/part4/scripts/build-image.sh
+++ 9.2D-resources/part4/scripts/build-image.sh
@@ -2,7 +2,3 @@
 ACR_NAME=myacrregistry123.azurecr.io
 VERSION_TAG=v1.0.0 
 docker-compose up --build -d
-# Tag the book_catalog Docker image with a version tag
-docker tag $ACR_NAME/book-catalog:latest $ACR_NAME/book-catalog:$VERSION_TAG
-# Tag the inventory_management Docker image with a version tag
-docker tag $ACR_NAME/inventory-management:latest $ACR_NAME/inventory-management:$VERSION_TAG
