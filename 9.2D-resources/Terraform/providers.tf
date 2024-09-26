22b0bac4ec7f3c9b92d90bfe561177a8e472ec42
Akhila Joseph <43929349+AkhilaJoseph@users.noreply.github.com>
2024-09-26 11:03:45 +1000

    Update providers.tf

--- 9.2D-resources/Terraform/providers.tf
+++ 9.2D-resources/Terraform/providers.tf
@@ -1,3 +1,8 @@
 provider "azurerm" {
   features {}
+  
+  subscription_id = var.subscription_id  # Make sure this is defined in variables.tf or passed from GitHub Actions
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
