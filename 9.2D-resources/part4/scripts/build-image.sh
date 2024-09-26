24242482bd46601b0d6b6feb2ae651f07964853d
Akhila Joseph <43929349+AkhilaJoseph@users.noreply.github.com>
2024-09-26 12:44:15 +1000

    Update build-image.sh

--- 9.2D-resources/part4/scripts/build-image.sh
+++ 9.2D-resources/part4/scripts/build-image.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 ACR_NAME=myacrregistry123.azurecr.io
 VERSION_TAG=v1.0.0 
+
+# Navigate to the correct directory where docker-compose is located
+cd ./9.2D-resources/part4/
+
 docker-compose up --build -d
