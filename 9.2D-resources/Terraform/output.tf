475ea95f1598506b4cdbbf4b9def4f3cb182018a
Akhila Joseph <43929349+AkhilaJoseph@users.noreply.github.com>
2024-09-26 14:11:16 +1000

    Update output.tf

--- 9.2D-resources/Terraform/output.tf
+++ 9.2D-resources/Terraform/output.tf
@@ -5,7 +5,4 @@ output "kubernetes_cluster_name" {
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive = true  # Mark as sensitive to hide in logs
-}
+
