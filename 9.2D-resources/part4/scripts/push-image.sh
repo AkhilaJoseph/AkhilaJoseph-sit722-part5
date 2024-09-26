3d792cd4dbfd601c5a5e48e87fd732266cd07af1
Akhila Joseph <43929349+AkhilaJoseph@users.noreply.github.com>
2024-09-26 13:00:36 +1000

    Update push-image.sh

--- 9.2D-resources/part4/scripts/push-image.sh
+++ 9.2D-resources/part4/scripts/push-image.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Push Docker images to the registry using environment variables for the registry URL
-docker tag book_catalog $CONTAINER_REGISTRY_URL/book_catalog:latest
-docker tag inventory_management $CONTAINER_REGISTRY_URL/inventory_management:latest
+# Set variables
+ACR_NAME=myacrregistry123.azurecr.io
+VERSION_TAG=v1.0.0  # Ensure this matches the one used in build-image.sh
 
-# Push the images to the registry
-docker push $CONTAINER_REGISTRY_URL/book_catalog:latest
-docker push $CONTAINER_REGISTRY_URL/inventory_management:latest
+# Push the book_catalog Docker images (latest)
+docker push $ACR_NAME/book-catalog:latest
+
+# Push the inventory_management Docker images (latest)
+docker push $ACR_NAME/inventory-management:latest
+
